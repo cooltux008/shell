ebce65294083ff5cbb83f4fa3c327f813f15457b
admin <liujun_live@msn.com>
2021-12-13 11:56:33 +0800

    image sync

--- harbor/image_sync.sh
+++ harbor/image_sync.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
+SRC_REGISTRY=src.repo.com
+DEST_REGISTRY=dest.repo.com:5000
+SRC_USER=hello
+SRC_PASSWORD=hello
+
 while read image 
 do
-		target_repo=$(echo $image|awk -F'/' '{print $2}')
-        docker run -i --rm \
-                -v /etc/hosts:/etc/hosts \
-                ananace/skopeo:latest sync \
-                --dest-tls-verify=false \
-                --src=docker \
-                --dest=docker \
-				$image 180.184.64.76/$target_repo
+TARGET_REPO=$(echo ${IMAGE}|awk -F'/' '{print $2}')
+docker run -i --rm \
+    -v /etc/hosts:/etc/hosts \
+    ananace/skopeo:latest sync \
+    --dest-tls-verify=false \
+    --src-creds=${SRC_USER}:${SRC_PASSWORD} \
+    --src=docker \
+    --dest=docker \
+    ${IMAGE} ${DEST_REGISTRY}/${TARGET_REPO}
 done < byte.txt
